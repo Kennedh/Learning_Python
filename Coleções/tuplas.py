ace99d9fc59b5edbd7850d1e36f946ea3a9568ec
kennedh <ken.cmarcellino@gmail.com>
2025-02-05 17:47:23 -0300

    Como utilizar tuplas

---
+++ Coleções/tuplas.py
@@ -0,0 +1,32 @@
+"""
+Tuplas são bastante parecidas com listas.
+
+Existem apenas duas diferenças
+
+1 - As tuplas são representadas por parênteses ()
+
+2 - As tuplas imutáveis: Isso significa que ela não muda, toda alteração gera uma nova tupla
+
+
+#Apesar de serem representadas por () podem receber dados sem ()
+
+tupla1 = (1, 2, 3, 4, 5, 6)
+print(tupla1)
+
+print(type(tupla1))
+
+tupla2 = 1, 2, 3, 4, 5, 6
+print(tupla2)
+
+print(type(tupla2))
+
+#Porém apenas um valor no () não é tupla
+
+tupla3 = (1)
+print(tupla3)
+
+print(type(tupla3))
+
+#Resumidamente a unica diferença de tupla para lista é que a tupla não se altera
+"""
+
