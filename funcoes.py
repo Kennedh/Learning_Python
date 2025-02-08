6962ba5bb76c2b11b281f108c39e45eb1dee630e
kennedh <ken.cmarcellino@gmail.com>
2025-02-08 18:57:26 -0300

    verifica_palindromo, fatorial e dia_semana_sbtr

--- funcoes.py
+++ funcoes.py
@@ -1,4 +1,6 @@
 #Escreva um programa que peça um número ao usuário e informe se ele é par ou ímpar.
+from os.path import split
+
 
 def par_ou_impar():
     numero = int(input("Digite um número: "))
@@ -18,4 +20,40 @@ def soma_naturais():
         natural = natural + i
     print(natural)
 
-soma_naturais()
+#Crie um programa que peça uma palavra e verifique se ela é um palíndromo (lida de trás para frente é igual).
+
+def verifica_palindromo():
+    palavra = input("Digite uma palavra: ").lower()
+    compara = palavra[::-1]
+    if compara == palavra:
+        print(f"A palavra {palavra} é um palindromo")
+    else:
+        print(f"A palavra {palavra} não é um palindromo")
+
+#Escreva uma função que receba um número N e retorne o seu fatorial (N!).
+
+def fatorial():
+    n = int(input("Digite um número: "))
+    fat = n
+    for i in range(n,1,-1):
+        fat = fat * (i-1)
+    print(f"O fatorial de {n} é {fat}")
+
+# Recebe um número n, dai esse número subtrai o dia atual e o dia resultado tem que trazer o dia da semana
+
+from datetime import datetime,timedelta
+
+dia_da_semana = { "Monday":"segunda", "Tuesday":"Terça",
+                  "Wednesday":"Quarta",  "Thursday":"quinta", "Friday":"sexta",
+                  "Saturday":"Sabado", "Sunday":"Domingo" }
+
+def dia_semana_sbtr():
+    n = int(input("Numero"))
+    hoje = datetime.today()
+    nd = hoje - timedelta(days=n)
+    hoje.strftime("%A")
+
+    print(hoje)
+    print(dia_da_semana[hoje.strftime("%A")])
+    print(nd)
+    print(dia_da_semana[nd.strftime("%A")])
