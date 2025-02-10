8d913191743697077bdf77e8cd114eb00cbc84f2
kennedh <ken.cmarcellino@gmail.com>
2025-02-10 20:24:23 -0300

    Tornei mais eficiente a entrada de valores

--- Coleções/exercicio1.py
+++ Coleções/exercicio1.py
@@ -5,18 +5,9 @@ lidos.
 
 lista = []
 
-num1 = int(input("Digite o primeiro valor: "))
-lista.append(num1)
-num2 = int(input("Digite o segundo valor: "))
-lista.append(num2)
-num3 = int(input("Digite o terceiro valor: "))
-lista.append(num3)
-num4 = int(input("Digite o quarto valor: "))
-lista.append(num4)
-num5 = int(input("Digite o quinto valor: "))
-lista.append(num5)
-num6 = int(input("Digite o sexto valor: "))
-lista.append(num6)
+for i in range(6):
+    num = int(input(f"Valor {i+1}: "))
+    lista.append(num)
 
 print(lista)
 
