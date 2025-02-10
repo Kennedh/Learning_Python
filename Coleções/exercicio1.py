97b64616378d25fe361e7388349a694a3070797e
kennedh <ken.cmarcellino@gmail.com>
2025-02-10 20:00:51 -0300

    Exercicio 1

---
+++ Coleções/exercicio1.py
@@ -0,0 +1,24 @@
+"""
+1. Crie um programa que lê 6 valores inteiros, armazene em uma lista e em seguida mostre na tela os valores
+lidos.
+"""
+
+lista = []
+
+num1 = int(input("Digite o primeiro valor: "))
+lista.append(num1)
+num2 = int(input("Digite o segundo valor: "))
+lista.append(num2)
+num3 = int(input("Digite o terceiro valor: "))
+lista.append(num3)
+num4 = int(input("Digite o quarto valor: "))
+lista.append(num4)
+num5 = int(input("Digite o quinto valor: "))
+lista.append(num5)
+num6 = int(input("Digite o sexto valor: "))
+lista.append(num6)
+
+print(lista)
+
+
+
